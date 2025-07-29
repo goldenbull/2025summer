931cc8b1d2f62a7e044a16cbd4aa457888196023
YQX14583 <2381656501@qq.com>
2025-07-29 17:37:43 +0800

    bugfix

--- sat/sudoku_to_sat.py
+++ sat/sudoku_to_sat.py
@@ -6,6 +6,8 @@ line_n = lines[n]
 list_cnf = []
 
 
+# 空白棋盘
+
 def get_box(r, c):
     return [(i, j)
             for i in range(r, r + 3)
@@ -32,6 +34,19 @@ for rng in ranges:
                 (r1, c1), (r2, c2) = rng[i], rng[j]
                 list_cnf.append((-r1 * 100 - c1 * 10 - v, -r2 * 100 - c2 * 10 - v))
 
+# 读入已有的数字
+curr_numbers = []
+for i in range(9):
+    for j in range(9):
+        c = line_n[9 * i + j]
+        if c != '.':
+            curr_numbers.append((i, j, int(c)))
+
+# 已填入的数字转为cnf
+for x, y, v in curr_numbers:
+    list_cnf.append((100 * (x + 1) + 10 * (y + 1) + v,))
+
+# 输出到cnf文件
 literals = set()
 for cnf in list_cnf:
     for v in cnf:
@@ -48,92 +63,3 @@ for clause in clauses:
     print(0, file=fout)
 
 fout.close()
-exit()
-
-# 格约束
-for i in range(1, 10):
-    for j in range(1, 10):
-        list_cnf.append([100 * i + 10 * j + k for k in range(1, 10)])
-        for k1 in range(1, 10):
-            for k2 in range(k1 + 1, 10):
-                list_cnf.append([-100 * i - 10 * j - k1, -100 * i - 10 * j - k2])
-
-# 行约束
-# 每行包含1-9
-for row in rows:
-    for k in range(1, 10):
-        list_cnf.append([100 * r + 10 * c + k for (r, c) in row])
-# 同一行不能包含重复数字
-for row in rows:
-    for i in range(9):
-        for j in range(i + 1, 9):
-            (r1, c1), (r2, c2) = row[i], row[j]
-            for k in range(1, 10):
-                list_cnf.append([-r1 * 100 - c1 * 10 - k, -r2 * 100 - c2 * 10 - k])
-
-# 列约束
-# 每列包含1-9
-for col in cols:
-    for k in range(1, 10):
-        list_cnf.append([100 * r + 10 * c + k for (r, c) in col])
-# 同一列不能包含重复数字
-for col in cols:
-    for i in range(9):
-        for j in range(i + 1, 9):
-            (r1, c1), (r2, c2) = col[i], col[j]
-            for k in range(1, 10):
-                list_cnf.append([-r1 * 100 - c1 * 10 - k, -r2 * 100 - c2 * 10 - k])
-
-# 盒子约束
-# 每九宫格包含1-9
-for box in boxes:
-    for k in range(1, 10):
-        list_cnf.append([100 * r + 10 * c + k for (r, c) in box])
-# 同一九宫格不能包含重复数字
-for box in boxes:
-    for i in range(9):
-        for j in range(i + 1, 9):
-            (r1, c1), (r2, c2) = box[i], box[j]
-            for k in range(1, 10):
-                list_cnf.append([-r1 * 100 - c1 * 10 - k, -r2 * 100 - c2 * 10 - k])
-
-# 百分号约束
-# 窗口包含1-9
-for box in pcf_boxes:
-    for k in range(1, 10):
-        list_cnf.append([100 * r + 10 * c + k for (r, c) in box])
-# 不能有重复数字
-for box in pcf_boxes:
-    for i in range(9):
-        for j in range(i + 1, 9):
-            (r1, c1), (r2, c2) = box[i], box[j]
-            for k in range(1, 10):
-                list_cnf.append([-r1 * 100 - c1 * 10 - k, -r2 * 100 - c2 * 10 - k])
-
-# 斜对角线
-line_range = [19, 28, 37, 46, 55, 64, 73, 82, 91]
-for k in range(1, 10):
-    list_cnf.append([10 * x + k for x in line_range])
-for i in range(len(line_range)):
-    for j in range(i + 1, len(line_range)):
-        x1 = line_range[i]
-        x2 = line_range[j]
-        for k in range(1, 10):
-            list_cnf.append([-x1 * 10 - k, -x2 * 10 - k])
-
-# input
-matrix = [[0] * 9 for i in range(9)]  # 全初始化为0
-for i in range(9):
-    for j in range(9):
-        c = line_n[9 * i + j]
-        if c != '.':
-            matrix[i][j] = int(c)
-        else:
-            matrix[i][j] = 0
-
-# 已填入数字
-for i in range(9):
-    for j in range(9):
-        k = matrix[i][j]
-        if k != 0:
-            list_cnf.append([100 * (i + 1) + 10 * (j + 1) + k])
